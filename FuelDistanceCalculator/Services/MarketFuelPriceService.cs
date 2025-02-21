d186e518ef2cebcc44f0265f8ab5ff0e05781e74
ottohartel <otto.haertel@gmail.com>
2025-02-21 20:15:43 +0100

    changing git ignore

--- FuelDistanceCalculator/Services/MarketFuelPriceService.cs
+++ FuelDistanceCalculator/Services/MarketFuelPriceService.cs
@@ -4,5 +4,7 @@ namespace FuelDistanceCalculator.Services;
 
 public class MarketFuelPriceService
 {
-
+    public List<string> GetGasstaions(string place, int Radius){
+        return new List<string>();
+    }
 }
